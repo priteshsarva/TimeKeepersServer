1d9c31bad7788e42c36b94d82c6f14c1500d6db3
priteshsarva <priteshsarva9825@gmail.com>
2025-09-19 23:43:29 +0530

    first step

---
+++ baseUrls.js
@@ -0,0 +1,30 @@
+const baseUrls = [
+   "https://watchhouse11.cartpe.in/",
+   "https://jilaniwatches11.cartpe.in/",
+   "https://saenterprise.cartpe.in/",
+   "https://thetimekeepers.cartpe.in/",
+   "https://watchcode1.cartpe.in/",
+   "https://uzer-watch.cartpe.in/",
+   "https://utstorewatch.cartpe.in/",
+   "https://watch-aqua.cartpe.in/",
+   "https://watch-adda.cartpe.in/",
+   "https://spwatches.cartpe.in/",
+   "https://watch-enterprise17.cartpe.in/",
+   "https://watchstore17.cartpe.in/",
+   "https://city-watch19.cartpe.in/",
+   "https://fashionpro123.cartpe.in/",
+   "https://watchgalleryin.cartpe.in/",
+   "https://timezone17.cartpe.in/",
+   "https://watchflex.cartpe.in/",
+   "https://mangoenterprise.cartpe.in/",
+   "https://zeewatches.cartpe.in/",
+   "https://maenterprise.cartpe.in/",
+   "https://eye-care.cartpe.in/",
+   "https://famwatch.cartpe.in/"
+];
+
+
+
+
+
+export { baseUrls }
