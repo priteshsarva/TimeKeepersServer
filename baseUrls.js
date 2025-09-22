7f9d8472ac0c996616aa2b930273de02a4f0cf6c
priteshsarva <priteshsarva9825@gmail.com>
2025-09-23 00:08:04 +0530

    DB updated on 2025-09-22 18:38:04

--- baseUrls.js
+++ baseUrls.js
@@ -16,8 +16,8 @@ const baseUrls = [
    // "https://watchgalleryin.cartpe.in/",
    // "https://timezone17.cartpe.in/",
    // "https://watchflex.cartpe.in/",
-   "https://mangoenterprise.cartpe.in/",
-   // "https://zeewatches.cartpe.in/",
+   // "https://mangoenterprise.cartpe.in/",
+   "https://zeewatches.cartpe.in/",
    // "https://maenterprise.cartpe.in/",
    // "https://eye-care.cartpe.in/",
    // "https://famwatch.cartpe.in/"
