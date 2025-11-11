2695b0d94f00cf16c7593b626620871d59bc5db5
priteshsarva <priteshsarva9825@gmail.com>
2025-11-11 13:10:16 +0530

    DB updated on 2025-11-11 07:40:15

--- baseUrls.js
+++ baseUrls.js
@@ -1,13 +1,4 @@
 const baseUrls = [
-   "https://maenterprise.cartpe.in/",
-   "https://eye-care.cartpe.in/",
-   "https://famwatch.cartpe.in/",
-   "https://wristtimess.cartpe.in/",
-   "https://watchclub.cartpe.in/",
-   "https://watchcultures.cartpe.in/",
-   "https://bismiwatches.cartpe.in/",
-   "https://awwaltime11.cartpe.in/",
-   "https://watchhouse11.cartpe.in/",
    "https://wristifyreseller.cartpe.in/",
    "https://watchfactorys.cartpe.in/",
    "https://watchkart.cartpe.in/",
@@ -30,7 +21,16 @@ const baseUrls = [
    "https://timezone17.cartpe.in/",
    "https://watchflex.cartpe.in/",
    "https://mangoenterprise.cartpe.in/",
-   "https://zeewatches.cartpe.in/"
+   "https://zeewatches.cartpe.in/",
+   "https://maenterprise.cartpe.in/",
+   "https://eye-care.cartpe.in/",
+   "https://famwatch.cartpe.in/",
+   "https://wristtimess.cartpe.in/",
+   "https://watchclub.cartpe.in/",
+   "https://watchcultures.cartpe.in/",
+   "https://bismiwatches.cartpe.in/",
+   "https://awwaltime11.cartpe.in/",
+   "https://watchhouse11.cartpe.in/"
 ];
 
 export { baseUrls };
