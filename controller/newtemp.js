55a6ca1dd8e039e3601ef452d8e4141f183f5ae6
priteshsarva <priteshsarva9825@gmail.com>
2025-11-30 17:53:44 +0530

    Rotated & saved activated

--- controller/newtemp.js
+++ controller/newtemp.js
@@ -171,13 +171,13 @@ async function fetchDataa(baseUrls) {
         baseUrls.push(baseUrls.shift());
 
         // 💾 Save updated rotation to baseUrls.js (live)
-        // const newFileContent = `const baseUrls = ${JSON.stringify(baseUrls, null, 3)};\n\nexport { baseUrls };`;
-        // try {
-        // fs.writeFileSync(baseUrlsPath, newFileContent, "utf-8");
-        // console.log("File written successfully!");
-        // } catch (err) {
-        // console.error("Failed to write baseUrls.js:", err);
-        // }
+        const newFileContent = `const baseUrls = ${JSON.stringify(baseUrls, null, 3)};\n\nexport { baseUrls };`;
+        try {
+        fs.writeFileSync(baseUrlsPath, newFileContent, "utf-8");
+        console.log("File written successfully!");
+        } catch (err) {
+        console.error("Failed to write baseUrls.js:", err);
+        }
 
         console.log(`✅ Rotated & saved baseUrls.js — next start will begin from: ${baseUrls[0]}`);
     }
